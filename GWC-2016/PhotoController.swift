368b1d72e8e860d4ea3133e1b93ef9758e723331
jvandrost <jvandrost@about.com>
2016-07-20 16:45:24 -0400

    added camera functionality to storyboard

---
+++ GWC-2016/PhotoController.swift
@@ -0,0 +1,48 @@
+//
+//  PhotoController.swift
+//  GWC-2016
+//
+//  Created by Joyce Van Drost on 7/20/16.
+//  Copyright © 2016 Daily Burn. All rights reserved.
+//
+
+import UIKit
+
+class PhotoController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var cameraAccess: UIButton!
+    @IBOutlet weak var photoLibrary: UIButton!
+    
+    
+    @IBAction func cameraAccessAction(sender: UIButton) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .Camera
+        presentViewController(picker, animated: true, completion: nil)
+    }
+    
+    @IBAction func photoLibraryAction(sender: UIButton) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .PhotoLibrary
+        presentViewController(picker, animated: true, completion: nil)
+    }
+    
+    /*
+        func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+            var imageData   = UIImagePNGRepresentation((info[UIImagePickerControllerOriginalImage] as? UIImage)!); dismissViewControllerAnimated(true, completion: nil)
+        }
+        
+ */
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            // Do any additional setup after loading the view, typically from a nib.
+        }
+        
+        override func didReceiveMemoryWarning() {
+            super.didReceiveMemoryWarning()
+            // Dispose of any resources that can be recreated.
+        }
+        
+        
+    }
